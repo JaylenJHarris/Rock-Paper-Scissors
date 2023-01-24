33840f50fd5aa2a595a25baf464bb11eebfc4cc7
Jaylen <jaylenh39@gmail.com>
2023-01-23 18:25:57 -0600

    Completed the playRound function

--- javascript2.js
+++ javascript2.js
@@ -1,5 +1,7 @@
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
+let compScore;
+let playerScore;
 
 function getComputerChoice(){
     let rps = ['rock', 'paper', 'scissors'];
@@ -19,9 +21,30 @@ function playRound(playerSelection, computerSelection){
         console.log('Tie game!');
     }else if(playerSelection === 'rock' && computerSelection === 'paper'){
         console.log('You lose! Paper beats rock.');
+        ++compScore;
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         console.log('You win! Rock beats scissors.');
+    }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        console.log('You lose! Scissors beats paper.');
+        ++compScore;
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log('You win! Paper beats rock.');
+    }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        console.log('You lose! Rock beats scissors.');
+        ++compScore;
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        console.log('You win! Scissors beats rock.');
     }else{
         console.log('Invalid input')
     }
+    
+
+
 }
+
+function game(){
+    for(let i=0; i < 5; i++){
+        playRound(playerSelection, computerSelection)
+    }
+}
+game()
