3cddaa65e27116324c657e510f316d17620fb48a
Jaylen Harris <jaylenh39@gmail.com>
2023-01-27 09:00:29 -0600

    Finished the console game for rock, paper, scissors

--- rpsGame.js
+++ rpsGame.js
@@ -1,4 +1,71 @@
 /*
-randomly generate the computer's choice
+randomly generate the computer's choice will do this using an array
 
-write a function that plays a single round of 
+write a function that plays a single round of rock paper scisors
+[
+    takes 2 parameters player selectionn and computer selection
+    compares the players selection vs the selection of the computer
+    and gives a point to the winning player
+]
+create the game function
+[
+    use the play round function inside 
+    create a loop to keep playing until either the player or the computer gets to 5 points
+        [
+            the loop needs to ask for the players input after every win
+        ]
+    reports the winner of the game 
+]
+*/
+
+let compScore = 0;
+let playerScore = 0;
+//const playerSelection = 'scissors';
+
+function commputerPlay(){
+    let rps = ['rock', 'paper', 'scissors'];
+    let comp = rps[Math.floor(Math.random() * rps.length)];
+    return(comp)
+}
+
+function singleRound(computerSelection, playerSelection){
+    if(playerSelection === computerSelection){
+        console.log('Tie game!');
+    }else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        console.log('You lose! Paper beats rock.');
+        compScore++;
+    }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        console.log('You win! Rock beats scissors.');
+        playerScore++;
+    }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        console.log('You lose! Scissors beats paper.');
+        compScore++;
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log('You win! Paper beats rock.');
+        playerScore++;
+    }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        console.log('You lose! Rock beats scissors.');
+        compScore++;
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        console.log('You win! Scissors beats rock.');
+        playerScore++;
+    }else{
+        console.log('Invalid input')
+    }
+}
+
+function game(){
+    for(let i=0;i < 5; i++){
+        const playerSelection = prompt('Will you choose rock, paper, or scissors?').toLowerCase();
+        const computerSelection = commputerPlay();
+        singleRound(computerSelection, playerSelection);
+    }
+    if(compScore < playerScore){
+        console.log('You win');
+    }else if(compScore > playerScore){
+        console.log('You lose')
+    }else if(compScore == playerScore){
+        console.log('Tie game')
+    }
+}
+game();
